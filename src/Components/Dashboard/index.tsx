7be9f95c839bb126f0012c0cb49c9f9113d21b09
Panuganti Nikhil Reddy <panugantinikhilreddy@gmail.com>
2025-02-21 12:44:23 +0530

    dashboard UI Revamp

--- src/Components/Dashboard/index.tsx
+++ src/Components/Dashboard/index.tsx
@@ -1,27 +1,67 @@
 import { useNavigate } from 'react-router-dom';
 import './dashboard.css';
 
+const gameFeatures = [
+    {
+        id: 1,title: "Tic Tac Toe" , navigation: "/tictactoe", type: 'GAME', enable: true
+     },
+     {
+         id: 2,title: "Ludo" , navigation: "/ludo", type: 'GAME' , enable: false
+     },
+     {
+         id: 3,title: "Chess" , navigation: "/chess", type: 'GAME' , enable: false
+     },
+     {
+         id: 4,title: "Tambola Ticket" , navigation: "/tambola-ticket", type: 'GAME' , enable: false
+     },
+     {
+         id: 5,title: "Tambola Number Generator" , navigation: "/tambola-number", type: 'GAME' , enable: false
+     },
+]
+
+const codeFeatures = [
+    
+    {
+        id: 1,title: "Todo App" , navigation: "/todo-app", type: 'CODE' , enable: false
+    },
+    {
+        id: 2,title: "Amazon Zoom Feature" , navigation: "/amazon-zoom", type: 'CODE' , enable: false
+    },
+    {
+        id: 3,title: "Loaders" , navigation: "/loaders", type: 'CODE' , enable: true
+    },
+]
+
+
 const Dashboard = () => {
     const navigate = useNavigate();
     return (
-        <div>
-            <div onClick={()=> navigate('/tictactoe')} >
-                Tic Tac Toe
+        <div className='dashboard-container' >
+            <div className='dashboard-games-container'>
+                <div>Wanna Play Games . Click on enable button to play your favourite game</div>
+                <div className='row-container'>
+                    {gameFeatures.length > 0
+                    &&
+                    gameFeatures.map((game,index)=>(
+                        <div className='button-div' key={`${game.id}-${index}`} >
+                            <button  onClick={()=>navigate(game.navigation)} disabled={!game.enable} > {game.title}</button>
+                        </div>
+                    )) 
+                    }
+                </div>
             </div>
-            <div>
-                Ludo
-            </div>
-            <div>
-                Amazon Image Zoom in feature
-            </div>
-            <div>
-                Todo App
-            </div>
-            <div onClick={() => navigate('/chess')} >
-                Chess
-            </div>
-            <div onClick={() => navigate('/loaders')} >
-                Loaders
+            <div className='dashboard-games-container'>
+                <div>Check out coding part</div>
+                <div className='row-container'>
+                    {codeFeatures.length > 0
+                    &&
+                    codeFeatures.map((code,index)=>(
+                        <div className='button-div' key={`${code.id}-${index}`} >
+                            <button onClick={()=>navigate(code.navigation)} disabled={!code.enable} > {code.title}</button>
+                        </div>
+                    )) 
+                    }
+                </div>
             </div>
         </div>
     )
